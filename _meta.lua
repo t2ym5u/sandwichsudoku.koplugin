06d77ad4814cd3bcc06358e1874cab3586f6927c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-08 14:04:33 +0200

    feat: i18n FR/EN translation + bump to 1.1.0

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "sandwichsudoku",
     fullname    = _("Sandwich Sudoku"),
     description = _("Sudoku with sandwich sum clues"),
-    version     = "1.0.1",
+    version     = "1.1.0",
 }
