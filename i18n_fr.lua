82e429fd81acc76da699fd2ff8429b60c43cced8
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:58:59 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,4 @@
 return {
-    ["Sandwich Sudoku"] = { fr = "Sudoku Sandwich" },
-    ["Sudoku with sandwich sum clues"] = { fr = "Sudoku avec indices de somme sandwich" },
+    ["Sandwich Sudoku"] = { fr = "Sudoku Sandwich", es = "Sudoku Sándwich", de = "Sandwich-Sudoku" },
+    ["Sudoku with sandwich sum clues"] = { fr = "Sudoku avec indices de somme sandwich", es = "Sudoku con pistas de suma sándwich", de = "Sudoku mit Sandwich-Summen-Hinweisen" },
 }
